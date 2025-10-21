c4defd8ffdfb50067daa2fabbf118a711481bc21
Bhavay Batra <btech10549.23@bitmesra.ac.in>
2025-10-22 01:39:55 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,5 +15,12 @@ app.use(express.static("public")); // to serve static files from public folder
 app.use(cookieParser()); // to parse cookies from the request headers
 
 
+// routes import
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+
 
 export {app}; // export default app
